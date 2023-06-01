fb49263dada684e22fb0411707d7c4209d97e0a5
STIA-Maverick <135238262+STIA-Maverick@users.noreply.github.com>
2023-06-01 23:58:50 +0800

    login.php

--- signup&login/login.php
+++ signup&login/login.php
@@ -1,8 +1,8 @@
 <?php
     // Database connection parameters
-    $host = "IP Address or localhost";
-    $username = "<input>";
-    $password = "<input>";
+    $host = "192.168.64.3";
+    $username = "test";
+    $password = "test12345";
     $database = "signup";
 
     // Create a database connection
@@ -23,9 +23,17 @@
 
         // Query the database to check if the user exists
         $sql = "SELECT * FROM users WHERE (username = '$loginUsername' OR email = '$loginUsername')";
+        $result = mysqli_query($conn, $sql);
 
-        // Redirect to another website
-        header("Location:<html of the main.php in TTRPG website folder>");
-        exit; // Ensure that the script stops executing after the redirect
+        if ($result && mysqli_num_rows($result) > 0) {
+            // User exists, perform further checks on the password, if necessary
+            // TODO: Add password validation logic here, e.g., comparing hashed passwords
+            
+            // Proceed with login
+            echo "<script>alert('Login successful!'); window.location.href = 'http://192.168.64.3/final_project/TTRPG%20website/main.php';</script>";
+        } else {
+            // User does not exist or credentials are incorrect
+            echo "<script>alert('Invalid username or password.'); window.location.href = 'http://192.168.64.3/final_project/signup&login/';</script>";
+        }
     }
 ?>
